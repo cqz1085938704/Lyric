29f19f31e1a5384368100a01e783f3cbd0dab2fc
caiyao's Mac <caiyaolq@163.com>
2015-08-06 14:54:18 +0800

    Initial Commit

---
+++ Lyric/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Lyric
+//
+//  Created by caiyao's Mac on 15/8/6.
+//  Copyright (c) 2015年 core's Mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
